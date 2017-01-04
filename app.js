13cb57f8740e186c3d1589e8ec3ce60f920dfd65
Eukavlin <brodin.elise@gmail.com>
2017-01-04 13:43:11 +0100

    Form style changed

--- app.js
+++ app.js
@@ -1,25 +1,82 @@
+// app global variable to maintain state
+const app = {
+  models: {},
+  views: {},
+  controllers: {}
+};
+
+// app people model
+app.models.people = {
+  get: function() {
+    return this.data;
+  },
+  add: function(newPerson) {
+    this.data.push(newPerson);
+    app.views.redraw();
+  },
+  // fake data to initialize example
+  data: [{
+      firstname: "Kenny",
+      surname: "McCormick",
+      age: 9,
+      height: 121
+  }, {
+      firstname: "Kyle",
+      surname: "Broslovski",
+      age: 9,
+      height: 125
+  }, {
+      firstname: "Stan",
+      surname: "Marsh",
+      age: 10,
+      height: 122
+  }, {
+      firstname: "Éric",
+      surname: "Cartman",
+      age: 10,
+      height: 119
+  }]
+};
+
+// app views
+app.views.redraw = function() {
+  // TODO : sauver app.people dans le localStorage
+  const people = app.models.people.get();
+  const tbody = document.querySelector("tbody");
+  const trContainer = document.createDocumentFragment();
+
+  // Remove tbody contents
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+  // Add new contents from people
+  for (let i = 0; i < people.length; i++) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>${people[i].firstname}</td><td>${people[i].surname}</td><td>${people[i].age}</td><td>${people[i].height}</td>`;
+    trContainer.appendChild(tr);
+  }
+  tbody.appendChild(trContainer);
+};
+
+// app controllers
+app.controllers.onSubmit = function(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const newPerson = {
+    surname: formData.get("sname"),
+    firstname: formData.get("fname"),
+    age: formData.get("age"),
+    height: formData.get("heights")
+  };
+  app.models.people.add(newPerson);
+};
+
+// DOM bindings
 const ready = function() {
-    const people = [
-      {firstname: "Kenny",surname: "McCormick", age: 9, gender: "Garçon" },
-      {firstname: "Kyle",surname: "Broslovski", age: 9, gender: "Garçon"},
-      {firstname: "Stan",surname: "Marsh", age: 10, gender: "Garçon"},
-      {firstname: "Éric",surname: "Cartman", age: 10, gender: "Garçon"}
-     ];
-    //
-    const tbody = document.querySelector("tbody");
-
-    let trContainer = document.createDocumentFragment();
-
-    for(let i = 0; i < people.length; i++) {
-      let tr = document.createElement("tr");
-      tr.innerHTML = `<td>${people[i].firstname}</td><td>${people[i].surname}</td><td>${people[i].age}</td><td>${people[i].gender}</td>`;
-        trContainer.appendChild(tr);
-    }
-
-    tbody.appendChild(trContainer);
+  // TODO charger app.people depuis le localStorage
+  // Initial drawing
+  app.views.redraw();
+  // On form submit
+  document.getElementById("addpeople").addEventListener("submit", app.controllers.onSubmit);
 }
-
-
-
-
 document.addEventListener("DOMContentLoaded", ready);
